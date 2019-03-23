b51d0426ebe14dc0aebb06b6a574c061ab4b465d
usama-jamil <usamajamil006@gmail.com>
2019-03-24 01:23:16 +0500

    React  Myreads

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import BookPage from './BookPage';
+import SearchPage from './SearchPage';
+
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? <SearchPage/> : <BookPage/>}
+      </div>
+    )
+  }
+}
+
+
+export default BooksApp
